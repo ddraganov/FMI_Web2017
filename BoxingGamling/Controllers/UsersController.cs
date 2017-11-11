2fc58e5ad89c586daa7e5cb9c1ff413fe897aa86
Dragomir Draganov <ddraganov@justgiving.com>
2017-11-11 23:49:26 +0200

    init

---
+++ BoxingGamling/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BoxingGamling.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public int Get()
+        {
+            return 42;
+        }
+
+        //[HttpGet]
+        //public int Test()
+        //{
+        //    return 43;
+        //}
+
+        //[HttpGet]
+        //public int Test1()
+        //{
+        //    return 1;
+        //}
+    }
+}
