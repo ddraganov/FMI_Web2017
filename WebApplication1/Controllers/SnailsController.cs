2b5b83545cf0418cb3d385dd83b3e9cc803df2bc
Dragomir Draganov <ddraganov@justgiving.com>
2017-11-15 11:17:47 +0200

    routes and controllers demonstration

---
+++ WebApplication1/Controllers/SnailsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SnailsController : ApiController
+    {
+        private static List<Snail> _snails = new List<Snail>();
+
+        [HttpGet]
+        public IEnumerable<Snail> GetSnails()
+        {
+            return _snails;
+        }
+
+        [HttpPost]
+        public Snail CreateSnail(Snail snail)
+        {
+            snail.Id = Guid.NewGuid().ToString();
+            _snails.Add(snail);
+            return snail;
+        }
+
+        [HttpGet]
+        public Snail GetSnail(string id)
+        {
+            return _snails.First(x => x.Id == id);
+        }
+
+        [HttpDelete]
+        public void Opi([FromUri]int page, [FromBody]int sth)
+        {
+            
+        }
+    }
+}
---
+++ WebApplication1/Models/Snail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Snail
+    {
+        public string Id { get; set; }
+
+        public double? ShellRadius { get; set; }
+
+        public double Length { get; set; }
+
+        public bool IsAlive { get; set; }
+    }
+}
